be030dbcb545c8c7425f50120778e82964d2bea0
Tiago <tiagojacintodev@gmail.com>
2024-07-24 11:42:43 +0100

    remove repeated types

--- apps/shared/src/core/Maybe.ts
+++ apps/shared/src/core/Maybe.ts
@@ -1,4 +1,4 @@
-import { type Nothing } from './Nothing';
+import { type Nil } from './Nil';
 
-export type Maybe<T> = T | Nothing;
+export type Maybe<T> = T | Nil;
 export type AsyncMaybe<T> = Promise<Maybe<T>>;
