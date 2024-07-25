6b6dca3f54cb6ee0214fc9722919888ffdd57a4d
Tiago <tiagojacintodev@gmail.com>
2024-07-25 12:07:00 +0100

    implement morgan middleware for request logging

---
+++ apps/backend/src/shared/infra/logging/morgan.ts
@@ -0,0 +1,55 @@
+import type express from 'express';
+import morgan from 'morgan';
+
+import { logger } from './';
+
+const infoIncomingMessage = (
+  tokens: morgan.TokenIndexer,
+  req: express.Request,
+  res: express.Response,
+) => {
+  return [tokens.method(req, res), tokens.url(req, res)].join(' ');
+};
+
+const errorIncomingMessage = (
+  tokens: morgan.TokenIndexer,
+  req: express.Request,
+  res: express.Response,
+) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'),
+    '-',
+    tokens['response-time'](req, res),
+    'ms',
+    res.locals.errorName + ':',
+    res.locals.errorMessage,
+  ].join(' ');
+};
+
+const infoStream = {
+  write: (message: string) => logger.info(message),
+};
+
+const errorStream = {
+  write: (message: string) => logger.error(message),
+};
+
+export const infoMorgan = morgan(infoIncomingMessage, {
+  stream: infoStream,
+  immediate: true,
+});
+
+export const errorMorgan = morgan(
+  (tokens, req: express.Request, res: express.Response) => {
+    return errorIncomingMessage(tokens, req, res);
+  },
+  {
+    stream: errorStream,
+    skip(_req: express.Request, res: express.Response): boolean {
+      return res.statusCode < 400;
+    },
+  },
+);
