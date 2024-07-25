0714bb835a460e1ecfcf4dc158ccb5f6a9e4de21
Tiago <tiagojacintodev@gmail.com>
2024-07-25 09:44:48 +0100

    add users route to router

--- apps/backend/src/shared/infra/apis/v1.ts
+++ apps/backend/src/shared/infra/apis/v1.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 
+import { userRouter } from '../../../modules/users/http/user.router';
+
 const v1Router = Router();
+
+v1Router.use('/users', userRouter);
 
 export { v1Router };
