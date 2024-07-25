769ed816cf5a37312a13aed9cbaa0272195e7c09
Tiago <tiagojacintodev@gmail.com>
2024-07-25 09:40:27 +0100

    add logging function to subscription handler

--- apps/backend/src/shared/domain/events/SubscriptionHandler.ts
+++ apps/backend/src/shared/domain/events/SubscriptionHandler.ts
@@ -1,3 +1,15 @@
-export interface SubscriptionHandler {
-  setupSubscriptions(): void;
+type EventLog = {
+  event: string;
+  success: boolean;
+  executedUseCase: string;
+};
+
+export abstract class SubscriptionHandler {
+  protected logEvent(log: EventLog) {
+    console.log(
+      `[${log.event}]: ${log.success ? 'Successfully executed' : 'Failed to execute'} ${log.executedUseCase} use case`,
+    );
+  }
+
+  protected abstract setupSubscriptions(): void;
 }
