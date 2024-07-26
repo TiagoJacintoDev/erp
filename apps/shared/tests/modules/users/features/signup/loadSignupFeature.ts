d86054ae8d0fb801c22e1b5c8110fe8c96a06a98
Tiago <tiagojacintodev@gmail.com>
2024-07-26 12:32:17 +0100

    chore(shared): change files location

--- apps/shared/tests/modules/users/features/signup/loadSignupFeature.ts
+++ apps/shared/tests/modules/users/features/signup/loadSignupFeature.ts
@@ -1,6 +1,6 @@
 import { defineFeature, loadFeature } from 'jest-cucumber';
 
-import { type FeatureTagFilter } from '../../../types/FeatureTagFilter';
+import { type FeatureTagFilter } from '../../../../types/FeatureTagFilter';
 import { ScenariosDefinitionCallbackFunction } from 'jest-cucumber/dist/src/feature-definition-creation';
 
 export function testSignupFeature(
