713902b4ec99ca0fd2e1f1bb81d393a671952567
Tiago <tiagojacintodev@gmail.com>
2024-07-27 14:33:39 +0100

    feat(frontend): setup query client

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,4 +1,5 @@
 import { createTheme, MantineProvider } from '@mantine/core';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@mantine/core/styles.css';
@@ -11,10 +12,14 @@ const theme = createTheme({
   cursorType: 'pointer',
 });
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MantineProvider theme={theme}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </MantineProvider>
   </React.StrictMode>,
 );
