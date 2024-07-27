d02584645ae079c8706a2563f883fa24723acd99
Tiago <tiagojacintodev@gmail.com>
2024-07-27 14:31:25 +0100

    feat(frontend): setup ui library

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,18 +1,20 @@
+import { createTheme, MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import '@mantine/core/styles.css';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <h1>Home</h1>,
-  },
-]);
+const router = createBrowserRouter([]);
+
+const theme = createTheme({
+  cursorType: 'pointer',
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <MantineProvider theme={theme}>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </React.StrictMode>,
 );
