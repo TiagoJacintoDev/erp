ca20c6d5ebcb96cef10c72dbb22e4e939f891505
Tiago <tiagojacintodev@gmail.com>
2024-08-07 11:04:58 +0100

    fix: type errors

--- apps/frontend/global.d.ts
+++ apps/frontend/global.d.ts
@@ -1,9 +1,4 @@
 import '@total-typescript/ts-reset';
 import '@sms/typescript-augments';
-import type Resources from './src/shared/i18n/resources';
 
-declare module 'i18next' {
-  interface CustomTypeOptions {
-    resources: Resources;
-  }
-}
+declare module 'i18next' {}
